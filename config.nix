ccbaba73156b1d46775ec3fa1e5d754d3a112b58
Nate Eagleson <neagleson@nxtbookmedia.com>
2020-07-23 01:24:40 -0400

    Get primitive config.nix almost-working
    Looks like I'll have to try this against a NixOS 20.03 box to have any
    success. :/

--- config.nix
+++ config.nix
@@ -1,7 +1,24 @@
-nixpkgs.config = {
-  allowUnfree = true;
-  chromium.enableWideVine = true;
-}
+{ config, pkgs, ... }:
 
-environment.systemPackages =
-  [ pkgs.chromium ];
+{
+  nixpkgs.config = {
+    allowUnfree = true;
+
+    chromium.enableWideVine = true;
+  };
+
+  environment.systemPackages = with pkgs; [
+    # Looks like Chromium is massively out of date on nixos 18, and is labeled
+    # as "broken". Somewhere around version 64 (current is apparently 84). I
+    # tried setting 'allowBroken = true;', per the error message I got, but
+    # that failed.
+    #
+    # Guess I'll have to try this on a NixOS 20 box (which doesn't seem to
+    # support Vagrant correctly).
+    #
+    # When I commented chromium out, installing just vim worked flawlessly.
+    # That's something, I guess.
+    chromium
+    vim
+  ];
+}
